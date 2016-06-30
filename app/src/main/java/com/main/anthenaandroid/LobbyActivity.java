08f32ed4be9f0778320f4b805752cbbc693c516d
Ten Zhi Yang <tzyinc@hotmail.com>
2016-06-30 10:07:23 +0800

    Changed color and fixed display problems on LobbyActivity

--- app/src/main/java/com/main/anthenaandroid/LobbyActivity.java
+++ app/src/main/java/com/main/anthenaandroid/LobbyActivity.java
@@ -1,6 +1,7 @@
 package com.main.anthenaandroid;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
@@ -17,7 +18,7 @@ import android.widget.FrameLayout;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class LobbyActivity extends AppCompatActivity {
+public class LobbyActivity extends Activity {
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -40,56 +41,7 @@ public class LobbyActivity extends AppCompatActivity {
     private boolean isRunner = true;
 
     RoomFinder rf;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,45 +49,31 @@ public class LobbyActivity extends AppCompatActivity {
 
         setContentView(R.layout.lobby_page);
 
-        mVisible = true;
-        mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
 
 
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-
-        findViewById(R.id.lockinbtn).setOnTouchListener(mDelayHideTouchListener);
 
         final Button stomperBtn = (Button) findViewById(R.id.stomperbtn);
         final Button runnerBtn = (Button) findViewById(R.id.runnerbtn);
-        runnerBtn.setBackgroundColor(Color.RED);
-        stomperBtn.setBackgroundColor(Color.GREEN);
-        Button lockBtn = (Button) findViewById(R.id.lockinbtn);
+        runnerBtn.setBackgroundColor(Color.GRAY);
+        stomperBtn.setBackgroundColor(Color.LTGRAY);
+        final Button lockBtn = (Button) findViewById(R.id.lockinbtn);
 
         stomperBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 isRunner = false;
-                stomperBtn.setBackgroundColor(Color.RED);
-                runnerBtn.setBackgroundColor(Color.GREEN);
+                stomperBtn.setBackgroundColor(Color.GRAY);
+                runnerBtn.setBackgroundColor(Color.LTGRAY);
             }
         });
         runnerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 isRunner = true;
-                stomperBtn.setBackgroundColor(Color.GREEN);
-                runnerBtn.setBackgroundColor(Color.RED);
+                stomperBtn.setBackgroundColor(Color.LTGRAY);
+                runnerBtn.setBackgroundColor(Color.GRAY);
             }
         });
 
@@ -144,12 +82,26 @@ public class LobbyActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i;
                 if (isRunner){
+
+                    System.out.println("Finding room");
+                    rf = new RoomFinder(RoomFinder.TYPE_RUNNER);
+                    Thread thread = new Thread(rf);
+                    thread.start();
+                    /*
                     i = new Intent(getApplicationContext(), runnerUI.class);
                     startActivity(i);
+                    */
                 }else{
+                    System.out.println("Finding room");
+                    rf = new RoomFinder(RoomFinder.TYPE_STOMPER);
+                    Thread thread = new Thread(rf);
+                    thread.start();
+                    /*
                     i = new Intent(getApplicationContext(), stomperUI.class);
                     startActivity(i);
+                    */
                 }
+                lockBtn.setBackgroundColor(Color.GRAY);
             }
         });
         /*
@@ -171,61 +123,9 @@ public class LobbyActivity extends AppCompatActivity {
         });*/
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        System.out.println("Finding room");
-        rf = new RoomFinder();
-        Thread thread = new Thread(rf);
-        thread.start();
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
-    }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
 
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
 
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-    }
 
     public void sendStomp (float x, float y){
         if (rf != null) {
